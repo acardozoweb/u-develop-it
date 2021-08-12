95a5a2a5f2263d403804c06c68f6da9b8f8ad7a7
anthonycc <anthonyccardozo@gmail.com>
2021-08-12 16:46:55 -0400

    testing client-side requests

--- server.js
+++ server.js
@@ -21,18 +21,52 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
+
   // querying the db to test connection
   // mthod runs the query and executes to callback 
   // with any resulting rows that match the query
-  db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
+  // aka GET ALL CANDIDATES
+//   db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+//   });
+
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 21`, (err, row) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [31, 'Robert', 'Burnham', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
 
   // CATCHALL ROUTE
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
