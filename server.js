aef8ed9d88ec5879172ac1ee43f8fd9d64d5b845
anthonycc <anthonyccardozo@gmail.com>
2021-08-12 14:55:03 -0400

    query to test database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '$uperLazy123',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+  // querying the db to test connection
+  // mthod runs the query and executes to callback 
+  // with any resulting rows that match the query
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+  // CATCHALL ROUTE
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
